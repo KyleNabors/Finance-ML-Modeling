f84bf8ffa1b94f2eb513d23cfad732fdaa2720d8
KyleNabors <kylewnabors@gmail.com>
2023-06-27 12:28:45 -0700

    2023 June 27 12:28pm

--- Learning LDA Optimised.py
+++ Learning LDA Optimised.py
@@ -1,11 +1,6 @@
 import os
 import nltk
-
-#Open Download Editor
-#nltk.download()
-
 import ssl
-nltk.download("stopwords") 
 import numpy as np
 import json
 import glob
@@ -19,65 +14,53 @@ import pyLDAvis
 import pyLDAvis.gensim
 import warnings
 
-#Json Functions
+nltk.download("stopwords")
+
 def load_data(file):
     with open(file) as f:
         data = json.load(f)
         return data
-    
+
 def write_data(file, data):
     with open(file, 'w', encoding="utf-8") as f:
         json.dump(data, f, indent=4)
-#Load Data
+
 stopwords = stopwords.words('english')
 print(stopwords)
 data = load_data("/Users/kylenabors/Documents/GitHub/MS-Thesis/Training Data/ushmm_dn.json")["texts"]
 print(data[0][0:90])
 
-#Remove Stopwords
-def lemmatization(texts, allowed_postags=["NOUN", "ADJ", "VERB", "ADV"]):
-    nlp = spacy.load("en_core_web_sm", disable=["parser", "ner"])
-    texts_out = []
-    for text in texts:
-        doc = nlp(text)
-        new_text = []
-        for token in doc:
-            if token.pos_ in allowed_postags:
-                new_text.append(token.lemma_)
-        final = " ".join(new_text)
-        texts_out.append(final)
-    return (texts_out)
+# Disable other components
+nlp = spacy.load("en_core_web_sm", disable=["parser", "ner"])
+allowed_postags=["NOUN", "ADJ", "VERB", "ADV"]
 
-#Lemmatize
+def lemmatization(texts, allowed_postags=allowed_postags):
+    docs = nlp.pipe(texts)
+    lemmatized_texts = []
+    for doc in docs:
+        lemmatized_texts.append(" ".join([token.lemma_ for token in doc if token.pos_ in allowed_postags]))
+    return lemmatized_texts
+
 lemmatized_texts = lemmatization(data)
 print (lemmatized_texts[0][0:90])
 
-#Remove Stopwords
 def gen_words(texts):
-    final = []
-    for text in texts:
-        new = gensim.utils.simple_preprocess(text, deacc=True)
-        final.append(new)
-    return(final)
+    return [gensim.utils.simple_preprocess(text, deacc=True) for text in texts]
 
-#Create Dictionary
 data_words = gen_words(lemmatized_texts)
 print(data_words[0][0:20])
-
-#Create Corpus
 id2word = corpora.Dictionary(data_words)
 
-corpus = []
-for text in data_words:
-    new = id2word.doc2bow(text)
-    corpus.append(new)
+# Using a generator for memory-efficient corpus creation
+def create_corpus(data_words):
+    for text in data_words:
+        yield id2word.doc2bow(text)
 
+corpus = list(create_corpus(data_words))
 print(corpus[0][0:20])
 
 word = id2word[[0][:1][0]]
 print(word)
-
-pyLDAvis.enable_notebook()
 
 lda_model = gensim.models.ldamodel.LdaModel(corpus=corpus,
                                             id2word=id2word,
@@ -87,6 +70,7 @@ lda_model = gensim.models.ldamodel.LdaModel(corpus=corpus,
                                             chunksize=100,
                                             passes=10,
                                             alpha='auto')
-                                            
+
 vis = pyLDAvis.gensim.prepare(lda_model, corpus, id2word, mds='mmds', R=30)
-pyLDAvis.save_html(vis, "/Users/kylenabors/Documents/GitHub/MS-Thesis/Visualisations/USHMM DN.html")
+
+
