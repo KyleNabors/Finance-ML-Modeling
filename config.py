7a7363cd5233ed1d6f9898326a840fe0a9780de5
KyleNabors <78065701+KyleNabors@users.noreply.github.com>
2023-07-10 12:06:41 -0700

    2023 July 10 12:06pm

--- config.py
+++ config.py
@@ -12,11 +12,12 @@ from pandas.plotting import table
 
 #Paramaters 
 year_ranges = [(2006, 2007), (2008, 2009), (2010, 2019), (2020, 2023)]
-keywords = ["interest", "inflation", "credit", "mortgage", "recession", "china", "trade"]
+keywords = ["interest", "inflation", "credit", "mortgage", "recession", "trade"]
 
 #Subfolders
 fed_funds_folder = "/Users/kylenabors/Documents/GitHub/MS-Thesis/Models/Fed Models/Graphs/Fed Funds"
 sp500_folder = "/Users/kylenabors/Documents/GitHub/MS-Thesis/Models/Fed Models/Graphs/SP500"
+sp500_change_folder = "/Users/kylenabors/Documents/GitHub/MS-Thesis/Models/Fed Models/Graphs/SP500 Change"
 four_model_graph_folders = [f"/Users/kylenabors/Documents/GitHub/MS-Thesis/Models/Fed Models/Four Models/Graphs/Period {i}" for i in range(1, 5)]
 four_models_datapath = "/Users/kylenabors/Documents/MS-Thesis Data/Database/Fed Data/Four Models"
 four_models_models_folder = "/Users/kylenabors/Documents/GitHub/MS-Thesis/Models/Fed Models/Four Models"
