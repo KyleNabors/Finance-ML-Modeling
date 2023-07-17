2fe7b61312602d691588d43c64fa132a44e328d5
KyleNabors <78065701+KyleNabors@users.noreply.github.com>
2023-07-16 19:10:09 -0700

    2023 July 16 07:10pm

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 #Import Libraries
 import os
+import os.path
 import sys
 import pandas as pd
 import statsmodels.api as sm
@@ -7,12 +8,23 @@ from statsmodels.tsa.arima.model import ARIMA
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 from pandas.plotting import table 
+import json
 
-#Varaibles
+# Load data from the JSON database
+def load_data(file):
+    with open(file, "r", encoding="utf-8") as f:
+        data = json.load(f)
+    return data
+
+# Write data to a JSON file
+def write_data(file, data):
+    with open(file, "w", encoding="utf-8") as f:
+        json.dump(data, f, indent=4)
+        
 
 #Paramaters 
 year_ranges = [(2006, 2007), (2008, 2009), (2010, 2019), (2020, 2023)]
-keywords = ["interest", "inflation", "credit", "trade", "bank"]
+keywords = ["interest", "inflation", "invest", "trade", "uncertain"]
 
 #Subfolders
 fed_models = "/Users/kylenabors/Documents/GitHub/MS-Thesis/Models/Fed Models"
@@ -23,10 +35,20 @@ four_model_graph_folders = [f"/Users/kylenabors/Documents/GitHub/MS-Thesis/Model
 four_models_datapath = "/Users/kylenabors/Documents/MS-Thesis Data/Database/Fed Data/Four Models"
 four_models_models_folder = "/Users/kylenabors/Documents/GitHub/MS-Thesis/Models/Fed Models/Four Models"
 
-
-
 #Files
 database = "/Users/kylenabors/Documents/MS-Thesis Data/Database/fed_database.json"
+
+if os.path.exists('/Users/kylenabors/Documents/MS-Thesis Data/Database/Fed Data/keyword_info_ts.csv'):
+    df_keyword_info_ts = pd.read_csv('/Users/kylenabors/Documents/MS-Thesis Data/Database/Fed Data/keyword_info_ts.csv')
+else:
+    df_keyword_info_ts = []
+    
+if os.path.exists('/Users/kylenabors/Documents/MS-Thesis Data/Database/Fed Data/keyword_freq_ts.json'):
+    keyword_freq_ts = load_data("/Users/kylenabors/Documents/MS-Thesis Data/Database/Fed Data/keyword_freq_ts.json")
+else:
+    keyword_freq_ts = []
+    
 fed_funds = pd.read_excel('/Users/kylenabors/Documents/MS-Thesis Data/Database/Fed Data/FedFundsRate.xlsx', sheet_name='Monthly')
-df_keyword_info_ts = pd.read_csv('/Users/kylenabors/Documents/MS-Thesis Data/Database/Fed Data/keyword_info_ts.csv')
 sp500 = pd.read_csv('/Users/kylenabors/Documents/MS-Thesis Data/Database/Market Data/Monthly SP.csv')
+
+#Varaibles
