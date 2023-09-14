7148b2913e601fbbba2c7fd22f7981732f1df364
KyleNabors <78065701+KyleNabors@users.noreply.github.com>
2023-09-14 11:54:27 -0700

    Bug Fixes

--- Model Analysis/Building Regressions.R
+++ Model Analysis/Building Regressions.R
@@ -14,3 +14,11 @@ library(tidyr)
 library(ggplot2)
 
 setwd("/Users/kylenabors/Documents/GitHub/Finance-ML-Modeling")
+
+tot <- read_csv("/Users/kylenabors/Documents/Database/Models/BERT Models/pivot_df_tot.csv")
+colnames(tot)[2] = "Topic_1"
+
+plt_stock_value <- ggplot(tot, aes(x=Timestamp, y=Topic_1)) +
+  geom_line()
+
+plt_stock_value
