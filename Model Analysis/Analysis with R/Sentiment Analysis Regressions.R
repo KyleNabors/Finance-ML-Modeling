781fdd3baab8ab5598c799644b44541cbc203d74
KyleNabors <78065701+KyleNabors@users.noreply.github.com>
2023-11-03 15:57:36 -0700

    Added Tone Analysis

--- Model Analysis/Analysis with R/Sentiment Analysis Regressions.R
+++ Model Analysis/Analysis with R/Sentiment Analysis Regressions.R
@@ -15,19 +15,19 @@ library(ggplot2)
 library(DataCombine)
 library(xts)
 
-setwd('/Users/kylenabors/Documents/Database/Models/Sentiment Analysis Models')
+#setwd('/Users/kylenabors/Documents/Database/Models/Sentiment Analysis Models')
 
-sentiment = read_csv('ECB_Speeches_vs_Fed_Speeches_advanced_sentiment_texts.csv')
+sentiment = read_csv('/Users/kylenabors/Documents/Database/Models/Sentiment Analysis Models/Fed_Speeches_vs_ECB_Speeches_advanced_sentiment_texts.csv')
 
-sent_reg = lm(net_1 ~ net_2, data = sentiment)
+sent_reg = lm(ECB_compound ~ Fed_compound, data = sentiment)
 summary(sent_reg)
 stargazer(sent_reg, out = 'Sentiment Regression.txt')
 
-plot_sentiment <- ggplot(sentiment, aes(x=net_1, y=net_2)) +
+plot_sentiment <- ggplot(sentiment, aes(x=ECB_compound, y=Fed_compound)) +
   geom_point()
 plot_sentiment
 
 plot_sentiment_time <- ggplot(sentiment, aes(x=monthly)) +
-  geom_line(aes(y=net_1)) + 
-  geom_line(aes(y=net_2)) 
+  geom_line(aes(y=ECB_compound)) + 
+  geom_line(aes(y=Fed_compound)) 
 plot_sentiment_time
--- Model Building/FinBERT.py
+++ Model Building/FinBERT.py
@@ -8,7 +8,7 @@ from sentence_transformers import SentenceTransformer
 from hdbscan import HDBSCAN
 from bertopic.representation import KeyBERTInspired, MaximalMarginalRelevance, TextGeneration
 from bertopic.vectorizers import ClassTfidfTransformer
-
+from nltk.tokenize import sent_tokenize
 from sklearn.feature_extraction.text import CountVectorizer
 from umap import UMAP
 import torch 
@@ -49,19 +49,21 @@ df = pd.read_csv(f"{Model_Folder}/{Model}_texts_long.csv")
 df = df[df['language'] == 'en']
 #Finbert 
 # Load model directly
-# from transformers import AutoTokenizer, AutoModelForSequenceClassification
-
-# tokenizer = AutoTokenizer.from_pretrained("ProsusAI/finbert")
-# model = AutoModelForSequenceClassification.from_pretrained("ProsusAI/finbert")
-
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from transformers import BertTokenizer, BertForSequenceClassification
+from transformers import pipeline
+
+tokenizer_1 = AutoTokenizer.from_pretrained("ProsusAI/finbert")
+model_1 = AutoModelForSequenceClassification.from_pretrained("ProsusAI/finbert")
 
 finbert = BertForSequenceClassification.from_pretrained('yiyanghkust/finbert-tone',num_labels=3)
-tokenizer = BertTokenizer.from_pretrained('yiyanghkust/finbert-tone')
+tokenizer_2 = BertTokenizer.from_pretrained('yiyanghkust/finbert-tone')
+nlp = pipeline("sentiment-analysis", model=finbert, tokenizer=tokenizer_2)
 
 labels = {0:'neutral', 1:'positive',2:'negative'}
 out = []
 sent_val = list()
+long = 0
 for index, row in df.iterrows():
     docs = row["segment"]
     timestamps = row['date']
@@ -69,15 +71,30 @@ for index, row in df.iterrows():
     title = row['title']
     docs = str(docs)
     
-    inputs = tokenizer(docs, return_tensors="pt", padding=True, truncation=True, max_length=511)
-    outputs = finbert(**inputs)[0]
-
+    inputs_1 = tokenizer_1(docs, return_tensors="pt", padding=True, truncation=True, max_length=511)
+    outputs = model_1(**inputs_1)[0]
     val = labels[np.argmax(outputs.detach().numpy())]  
     sent_val.append(val)
-    out.append([timestamps, title, type, docs, val])
+    
+    segments = sent_tokenize(docs, language='english')
+    results = []
+    for segment in segments:
+        if 1800 < len(segment):
+            continue
+        long = max(long, len(segment))
+        result = nlp(segment)
+        if result[0]['label'] == 'Positive':
+            m = 1
+        elif result[0]['label'] == 'Negative':
+            m = -1
+        else:
+            m = 0
+        results.append(m)
+    
+    mean_value = sum(results) / len(results) 
 
-
-
-df_out = pd.DataFrame(out, columns=["date", "title", "type", "segment", "compound"])
+    out.append([timestamps, title, type, docs, val, mean_value])
+df_out = pd.DataFrame(out, columns=["date", "title", "type", "segment", "sentiment", "tone"])
+print(df_out.head())
 
 df_out.to_csv(f"{finbert_models}/{Body}_{Model}_finbert model.csv")  
