ade1ab9d8227dfc47bca3303e1e1c1ed2ef47423
KyleNabors <78065701+KyleNabors@users.noreply.github.com>
2024-02-08 14:38:44 -0800

    Added TS Analysis

--- Temp.py
+++ Temp.py
@@ -6,32 +6,11 @@ from matplotlib import ticker
 import numpy as np
 
 
-df = pd.read_csv("/Users/kylenabors/Documents/Database/Market Data/Unemployment/Unemployment.csv")
-df = df.rename(columns = {'DATE':'date', 'UNRATE':'unemployment'})
-print(df.head())
 
-
-df.to_csv("/Users/kylenabors/Documents/Database/Market Data/Unemployment/Unemployment.csv", index = None, header=True)
-
-
-# directory = '/Users/kylenabors/Downloads/Bloomberg Data-selected'
-
-# # Check if the directory exists
-# if not os.path.exists(directory):
-#     print(f"The directory {directory} does not exist.")
-#     sys.exit()
-
-# for filename in os.listdir(directory):
-#     if filename.endswith('.xlsx'): 
-#         path = os.path.join(directory, filename)
-#         print(path)
-#         try:
-#             df = pd.read_excel(path)
-#             # Change the file extension to .csv
-#             csv_filename = os.path.splitext(filename)[0] + '.csv'
-#             csv_path = os.path.join(directory, csv_filename)
-#             df.to_csv(csv_path, index = None, header=True)
-#         except PermissionError:
-#             print(f"Permission denied for file {path}.")
-#         except Exception as e:
-#             print(f"An error occurred while processing file {path}: {e}")
+market = pd.merge(market, sp500, on='date', how='outer')
+market = pd.merge(market, emini, on='date', how='outer')
+market = pd.merge(market, eurodollar, on='date', how='outer')
+market = pd.merge(market, fedfutures, on='date', how='outer')
+market = pd.merge(market, vix, on='date', how='outer')
+market = pd.merge(market, unemployment, on='date', how='outer')
+market = pd.merge(market, inflation, on='date', how='outer')
