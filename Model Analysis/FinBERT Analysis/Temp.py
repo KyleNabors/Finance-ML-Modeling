e3013f23e5d1f0b5051134ac8d09c1cb1ffc3e76
KyleNabors <78065701+KyleNabors@users.noreply.github.com>
2024-04-09 16:10:16 -0700

    Updated Figure Titles

--- Model Analysis/FinBERT Analysis/Temp.py
+++ Model Analysis/FinBERT Analysis/Temp.py
@@ -5,61 +5,32 @@ import matplotlib.pyplot as plt
 from matplotlib import ticker
 import numpy as np
 
-
-ecb_speech1 = pd.read_csv(
-    "/Users/kylenabors/Documents/Database/Training Data/ECB/Monetary policy decisions/Warin_Sanger_ECB.csv",
-    encoding_errors="ignore",
-    sep=";",
+# Define the file paths
+meeting_minutes_path = (
+    "/Users/kylenabors/Documents/Database/Training Data/fed/minutes/meeting_minutes.csv"
 )
+communications_path = "/Users/kylenabors/Downloads/communications.csv"
 
-ecb_speech2 = pd.read_excel(
-    "/Users/kylenabors/Documents/Database/Training Data/ECB/Monetary policy decisions/Monetary policy decisions.xlsx"
+# Read the meeting minutes data
+mins = pd.read_csv(meeting_minutes_path)
+print(mins.columns)
+
+# Read the communications data
+mins_new = pd.read_csv(communications_path)
+mins_new = mins_new[mins_new["Type"] == "Minute"]
+mins_new = mins_new.rename(
+    columns={"Date": "meeting_date", "Release Date": "release_date", "Text": "text"}
 )
+mins_new = mins_new[mins_new["release_date"] > "2023-07-05"]
+print(mins_new.columns)
 
+# combine the two dataframes and combine the text columns
+minutes = pd.concat([mins, mins_new])
+print(minutes.columns)
 
-ecb_speech1 = ecb_speech1[["date", "firstPart"]]
-ecb_speech1 = ecb_speech1.rename(columns={"firstPart": "segment"})
+print(minutes["meeting_date"].min())
+print(minutes["meeting_date"].max())
 
-
-ecb_speech2 = ecb_speech2[["date", "title", "segment"]]
-
-# convert date from yyyymmdd to yyyy-mm-dd
-ecb_speech2["date"] = ecb_speech2["date"].astype(str)
-ecb_speech2["date"] = ecb_speech2["date"].apply(
-    lambda x: x[:4] + "-" + x[4:6] + "-" + x[6:]
+minutes.to_csv(
+    "/Users/kylenabors/Documents/Database/Training Data/fed/minutes/meeting_minutes.csv"
 )
-
-
-print(ecb_speech2.head())
-
-
-ecb_speech = pd.concat([ecb_speech1, ecb_speech2], axis=0)
-
-print(ecb_speech.head())
-
-ecb_speech = ecb_speech.sort_values(by="date")
-
-
-ecb_speech.to_csv(
-    "/Users/kylenabors/Documents/Database/Training Data/ECB/Monetary policy decisions/Monetary_policy_decisions.csv",
-    index=False,
-)
-
-
-ecb_speech = ecb_speech.resample("Q", on="date").mean().reset_index()
-
-filter_df = ecb_speech.copy(deep=True)
-filter_df = filter_df[["date", "ecb_speech_sentiment"]]
-
-cycle, trend = sm.tsa.filters.hpfilter(filter_df["ecb_speech_sentiment"], 1600)
-
-filter_df["ecb_speech_sentiment_cycle"] = cycle
-filter_df["ecb_speech_sentiment_trend"] = trend
-
-filter_df = filter_df[["date", "ecb_speech_sentiment_cycle"]]
-ecb_speech = ecb_speech.drop(columns=["ecb_speech_sentiment"])
-filter_df = filter_df.rename(
-    columns={"ecb_speech_sentiment_cycle": "ecb_speech_sentiment"}
-)
-ecb_speech = pd.merge(ecb_speech, filter_df, on="date", how="left")
-ecb_speech = ecb_speech.groupby("date").mean().reset_index()
